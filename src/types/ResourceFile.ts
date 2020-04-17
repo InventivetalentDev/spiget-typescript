bc838fbd88ee5b958a44170bcfbf729e2d12a40f
inventivetalent <mail@inventivetalent.org>
2020-04-17 14:53:07 +0200

    add fullUrl

--- src/types/ResourceFile.ts
+++ src/types/ResourceFile.ts
@@ -1,4 +1,9 @@
 import ResourceFileBase from "../generated_types/ResourceFileBase";
 
 export default class ResourceFile extends ResourceFileBase {
+
+    fullUrl() {
+        return this.url.startsWith("http") ? this.url : (this._spiget.__spigotMcUrl + this.url);
+    }
+
 }
