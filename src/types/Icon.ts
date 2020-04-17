68a9af4cf99dbf2903391d84a451dd8e133b81e1
inventivetalent <mail@inventivetalent.org>
2020-04-17 14:34:42 +0200

    make fullUrl dependant on the url content, since avatars seem to be served from gravatar

--- src/types/Icon.ts
+++ src/types/Icon.ts
@@ -3,7 +3,7 @@ import IconBase from "../generated_types/IconBase";
 export default class Icon extends IconBase {
 
     fullUrl() {
-        return this._spiget.__spigotMcUrl + this.url;
+        return this.url.startsWith("http") ? this.url : (this._spiget.__spigotMcUrl + this.url);
     }
 
 }
