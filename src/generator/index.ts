a53dbab525c045a1003756525e2cfe46fde54b8e
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-07-03 14:01:08 +0300

    Generate the typed classes from the swagger file

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -1,4 +1,15 @@
+import { definitions } from "../../spiget-documentation/swagger.json";
+import { Definition } from "./swagger/definition";
+import { TypeGenerator } from "./type";
+
 export function generator() {
-    // TODO Generate the type classes
+    // Generate the type classes
+    for (const name of Object.keys(definitions)) {
+        const definition: Definition = definitions[name];
+
+        const generator = new TypeGenerator(name, definition);
+        generator.generate();
+    }
+
     // TODO Generate the path functions
 }
