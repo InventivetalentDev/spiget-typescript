ad525bce3f904d3b87cd6c484a6702a94cfba5af
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-07-03 12:12:23 +0300

    TODO: A list that includes all of the implementations imports to write them into _imports.txt

--- src/generator/implementation.ts
+++ src/generator/implementation.ts
@@ -1,6 +1,8 @@
 import { Definition } from "./swagger/definition";
 import { Generator } from "./generator";
 
+// TODO A list of all implementations created
+
 export class ImplementationGenerator implements Generator {
 
     constructor(
@@ -10,6 +12,7 @@ export class ImplementationGenerator implements Generator {
     public generate() {
         // TODO Check if the implementation file exists
         // TODO If not then generator the default implementation
+        // TODO add the implementation to the list
     }
 
     private write() {
