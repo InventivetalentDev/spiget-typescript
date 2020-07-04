60bcdfe1412f36c19b1ba1c44dc90e5034aa18b8
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-07-04 06:28:13 +0300

    Document how the new generator uses the utilities

--- src/generator/util.ts
+++ src/generator/util.ts
@@ -1,18 +1,35 @@
 import { Schema } from "./swagger/schema";
 
+/**
+ * The source directory to generate the types inside it
+ */
 const SOURCE_DIR = "./src"
+
+/**
+ * Directory of generated types classes
+ */
 export const GENERATED_TYPES_DIR = `${SOURCE_DIR}/generated_types`;
+
+/**
+ * Directory of generated implementations
+ */
 export const TYPES_DIR = `${SOURCE_DIR}/types`;
 
-export function buildWithNewLines(array: string[], endWithNewLine = true): string {
-    if (array.length <= 0) {
+/**
+ * Transforms an array of string into a combined string with new lines
+ * 
+ * @param source A source to build the combined string with it
+ * @param endWithNewLine Finish the string with a new line or not
+ */
+export function buildWithNewLines(source: string[], endWithNewLine = true): string {
+    if (source.length <= 0) {
         return "";
     }
 
     let result = "";
-    for (let i = 0; i < array.length; i++) {
-        result += array[i];
-        if (i === array.length - 1 && !endWithNewLine) {
+    for (let i = 0; i < source.length; i++) {
+        result += source[i];
+        if (i === source.length - 1 && !endWithNewLine) {
             continue;
         }
         result += "\n";
@@ -21,9 +38,15 @@ export function buildWithNewLines(array: string[], endWithNewLine = true): strin
     return result;
 }
 
-// https://stackoverflow.com/a/2970667/6257838
-export function camelize(value: string): string {
-    return value.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+/**
+ * Transform a name into camelized form
+ * 
+ * Reference: https://stackoverflow.com/a/2970667/6257838
+ * 
+ * @param source A name to camelize it 
+ */
+export function camelize(name: string): string {
+    return name.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
         if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
         return index === 0 ? match.toLowerCase() : match.toUpperCase();
     });
