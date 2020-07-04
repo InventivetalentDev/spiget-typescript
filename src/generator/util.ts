f71e53803158055b498ec1da2bd78cd2ca3b2337
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-07-04 06:31:26 +0300

    Since we don't need a white space during the tranform into a camelized form

--- src/generator/util.ts
+++ src/generator/util.ts
@@ -47,7 +47,7 @@ export function buildWithNewLines(source: string[], endWithNewLine = true): stri
  */
 export function camelize(name: string): string {
     return name.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-        if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+        if (+match === 0) return "";
         return index === 0 ? match.toLowerCase() : match.toUpperCase();
     });
 }
