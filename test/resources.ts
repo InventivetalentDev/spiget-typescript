ce015a9c5f0be294bd62598da4a79869a54bf576
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-07-05 09:34:19 +0300

    Convert test scripts from javascript to typescript

--- test/resources.ts
+++ test/resources.ts
@@ -1,13 +1,11 @@
-const assert = require("assert");
-const should = require("chai").should;
-const expect = require("chai").expect;
+import { expect } from "chai";
+import { Spiget, Pagination } from "../index";
 
-const {Spiget, Pagination} = require("../index");
-const spiget = new Spiget();
+describe("RESOURCES", () => {
+    const spiget = new Spiget();
 
-describe("RESOURCES", function () {
-    describe("#getResources()",function () {
-        it("should return an array of Resource objects",async()=>{
+    describe("#getResources()", () => {
+        it("should return an array of Resource objects", async () => {
             let resources = await spiget.getResources()
             expect(resources).to.be.an("Array")
             expect(resources).to.not.be.empty;
@@ -17,10 +15,11 @@ describe("RESOURCES", function () {
             expect(resource).to.include.all.keys("id", "name", "tag");
             expect(resource.name).to.not.be.empty;
             expect(resource.tag).to.not.be.empty;
-        })
+        });
     });
-    describe("#getResources(size=1)",function () {
-        it("should return an array with a single Resource object",async()=>{
+
+    describe("#getResources(size=1)", () => {
+        it("should return an array with a single Resource object", async () => {
             let resources = await spiget.getResources(new Pagination(1))
             expect(resources).to.be.an("Array")
             expect(resources).to.have.lengthOf(1);
@@ -29,6 +28,7 @@ describe("RESOURCES", function () {
             expect(resource).to.be.an("object")
             expect(resource).to.include.all.keys("id", "name", "tag");
             expect(resource.name).to.not.be.empty;
-        })
+        });
     });
+
 });
